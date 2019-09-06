56f5b568fc039360e05134a471a5ee4e668420a2
achen173 <54816128+achen173@users.noreply.github.com>
2019-09-05 20:46:18 -0400

    push from pycharm 9/5/2019

--- Main.py
+++ Main.py
@@ -1,40 +1,42 @@
+import bs4 as bs
 import datetime as dt
-import matplotlib.pyplot as plt
-from matplotlib import style
-from mpl_finance import candlestick_ohlc
-import matplotlib.dates as mdates
+import os
 import pandas as pd
 import pandas_datareader.data as web
-style.use('ggplot')
+import pickle
+import requests
+def sp500_tickers():    # gets all SP 500 symbols and writes it in a file
+    resp = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'class':'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        ticker = ticker[:-1]
+        tickers.append(ticker)
+    with open('sp500','wb') as f:
+        #Pickle data is opaque, binary data
+        pickle.dump(tickers, f)
+    return tickers
 
-df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()# moving average
-df_ohlc = df['Adj Close'].resample('10D').ohlc()    #ohlc average of ever 10 days of data
-df_volume = df['Volume'].resample('10D').sum()
-df_ohlc.reset_index(inplace=True)
-df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
-print(df)
-#df[['Adj Close','100ma']].plot()
-#print(df_ohlc.values[0])
-#print(df_ohlc.head())
-ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
-ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
-ax1.xaxis_date()
-candlestick_ohlc(ax1, df_ohlc.values, width=1, colorup='g')
-ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
-plt.show()
-'''
-Ctrl+Alt+L      --format
-Ctrl+/          --commenting
+def get_data_from_yahoo(reload_sp500=False):    # reads the symbol and gets basic
+    # measurements for each one stoing it in a different file (for now it is changed to the first 25
 
-df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
-df['Adj Close'].plot()
-print(df[['Open','High']].head())
-df.plot()
-plt.show()
-
-
-df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
-df['100ma'] = df['Adj Close'].rolling(window=100).mean()# moving average
-print(df.head())
-'''
+    if reload_sp500:
+        tickers = sp500_tickers()
+    else:
+        with open('sp500','rb') as f:
+            tickers = pickle.load(f)
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+    start = dt.datetime(2000,1,1)
+    end = dt.datetime(2016, 12, 31)
+    for ticker in tickers[:20]:
+        print(tickers)
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+#sp500_tickers()
+get_data_from_yahoo()
